1756db201ba2b3d185089cf804b3c84516bfdaeb
Daniel Quinteros <quinterosm.daniel@gmail.com>
2025-03-04 21:42:55 -0300

    add vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), netlifyPlugin()],
 })
